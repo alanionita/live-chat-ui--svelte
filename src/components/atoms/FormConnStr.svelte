c3c31bcf08591c2b81c8a6a212df7ad9d23a2346
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-05-27 20:09:47 +0100

    FEAT (comp/FormConnStr): adds svelte component for the Connection String submition

---
+++ src/components/atoms/FormConnStr.svelte
@@ -0,0 +1,20 @@
+<script>
+	function validate() {
+		console.log("I'm the validate() function");
+        return;
+    }
+</script>
+
+<form on:submit>
+	<label for="connectionStr">
+		<p>Add WebSocket connect string</p>
+	</label>
+	<input
+		type="text"
+		name="connectionStr"
+		id="connectionStr"
+		placeholder="Please enter a connection string"
+        on:input={validate}
+    />
+	<button type="submit"> Connect </button>
+</form>
