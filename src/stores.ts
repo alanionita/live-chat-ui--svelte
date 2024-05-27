dc52687ab51cad6a6e7433d9526129b55cf808f8
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-05-27 22:38:19 +0100

    REFACTOR (stores) : adds persistent stores system

--- src/stores.ts
+++ src/stores.ts
@@ -1,4 +1,35 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
+const lsLabelWsConnString = "LS_WS_CN_STR";
+const lsLabelUserName = "LS_UN";
 
-export const wsConnString = writable('');
+let localWsConnString = '';
+let localUserName = '';
+
+if (browser) {
+    localWsConnString = localStorage.getItem(lsLabelWsConnString) || '';
+}
+
+const wsConnString = writable(localWsConnString);
+wsConnString.subscribe(val => {
+    if (browser) {
+        localStorage.setItem(lsLabelWsConnString, val);
+    }
+});
+
+if (browser) {
+    localUserName = localStorage.getItem(lsLabelUserName) || '';
+}
+
+export const userName = writable(localUserName);
+userName.subscribe(val => { 
+    if (browser) { 
+        localStorage.setItem(lsLabelUserName, val) 
+    } 
+});
+
+export default {
+    wsConnString,
+    userName
+}
