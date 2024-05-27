f21beabcc2f04e9143c3feaccb369692659e512f
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-05-27 20:10:14 +0100

    FEAT (page/home): implements connection string form and adds submit function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import FormConnStr from '../components/atoms/FormConnStr.svelte';
+
+	function handleSubmit(event: Event) {
+        const target = event.target as HTMLFormElement;
+
+		if (!target) throw Error('Target must be present');
+		if (target) {
+			const formData = new FormData(target);
+
+			const data: Record<string, any> = {};
+			for (let field of formData) {
+				const [key, value] = field;
+				data[key] = value;
+			}
+			console.log('Submit data ::', data);
+		}
+	}
+</script>
+
+<h1>Mini Live Chat UI</h1>
+
+<FormConnStr on:submit={handleSubmit}></FormConnStr>
